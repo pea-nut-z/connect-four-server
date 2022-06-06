6a65e2eb76027f4d490325daab2d41224da25a1e
pea-nut-z <topeanutz@gmail.com>
2022-06-06 19:01:51 -0400

    added missing code

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const server = require("socket.io")(http, {
   },
 });
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
